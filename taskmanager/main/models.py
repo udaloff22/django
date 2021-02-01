b767204c616e8ff7c124793a2d60788ddc3cb145
udaloff22 <alex.udalov@mail.ru>
2021-02-01 23:44:54 +0300

    ver2

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    title = models.CharField('naming', max_length=50)
+    text = models.TextField('description')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html',
+        {
+            'title' : 'Tha Main Page!',
+            'tasks' : tasks,
+        })
 
 def about(request):
     return render(request, 'main/about.html')
